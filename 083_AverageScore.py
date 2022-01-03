23ce7b7355c8c9a9a21133effe02f4551160c084
eastlaw981002 <>
2022-01-03 05:39:54 +0000

    220103_1

---
+++ 083_AverageScore.py
@@ -0,0 +1,9 @@
+#국어 영어 수학 과학 점수 를 입력하고 평균을 출력하는 프로그램을 만드세요
+Korean = int(input())
+English = int(input())
+Math = int(input())
+Science = int(input())
+
+Average = (Korean + English + Math + Science) / 4
+
+print("4과목의 평균 = ", Average)
