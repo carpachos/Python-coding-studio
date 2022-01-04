a90099ca8ec2ce26c29ac97244cdfd6a5f2d3aea
eastlaw981002 <>
2022-01-04 04:52:59 +0000

    220104

---
+++ 113_PrintManylines.py
@@ -0,0 +1,11 @@
+#문자열을 출력하라
+#'Python' is a "programming language"
+#that lets you quickly
+#and
+#integrate systems more effectively
+
+print("'Python' is a", end= ' ')
+print('"programming language"')
+print("that lets you quickly")
+print("and")
+print("integrate systems more effectively")
